79b8dc377ad70c670c5d29d0489a9e9b38ad7455
Marcos Martins <30351698+marcosmn@users.noreply.github.com>
2020-06-23 20:16:50 -0300

    Implementando controle de ações

--- aula6/App.cpp
+++ aula6/App.cpp
@@ -10,6 +10,32 @@ int App::run(int argc, char* argv[])
 {
     if (argc == 1)
     {
-      return show_usage();
-      }
+        return show_usage();
+    }
+    
+    std::string action = argv[1];
+    if (action == "add")
+    {
+        if (argc == 2)
+        {
+            add();
+        }
+        else
+        {
+            add(argv[2]);
+        }
+    }
+    else if (action == "list")
+    {
+        list_messages();
+    }
+    else if (action == "search")
+    {
+    }
+    else
+    {
+        return show_usage();
+    }
+    
+    return 0;
 }
