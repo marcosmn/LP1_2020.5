07b19958082cfd9d108bb224b26c08846957f773
Marcos Martins <30351698+marcosmn@users.noreply.github.com>
2020-06-23 20:44:52 -0300

    Implementando metodos add e write

--- aula6/Diario.cpp
+++ aula6/Diario.cpp
@@ -1,15 +1,17 @@
 #include "Diario.h"
 
-Diario::Diario(const std::string& filename)
+Diario::Diario(const std::string& fn) : filename(fn)
 {
 }
 
 void Diario::add(const std::string& mensagem)
 {
-    
+    capacidade_mensagens++;
 }
 
 void Diario::write()
 {
-    
+    for (size_t i = 0; i < diario.tamanho_mensagens; ++i) {
+        diario.mensagens[i] = mensagem;
+    }
 }
