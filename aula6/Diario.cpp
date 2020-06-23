73b3599d871c701770bbc4c2c51cefe7f158a901
Marcos Martins <30351698+marcosmn@users.noreply.github.com>
2020-06-23 20:47:39 -0300

    Atualizando método de add

--- aula6/Diario.cpp
+++ aula6/Diario.cpp
@@ -7,11 +7,9 @@ Diario::Diario(const std::string& fn) : filename(fn)
 void Diario::add(const std::string& mensagem)
 {
     capacidade_mensagens++;
+    diario.mensagens[capacidade_mensagens] = mensagem;
 }
 
 void Diario::write()
 {
-    for (size_t i = 0; i < diario.tamanho_mensagens; ++i) {
-        diario.mensagens[i] = mensagem;
-    }
 }
