feac0602a0cf6b44865cb72402af10c6c3111929
MarcosNobrega <marcosmnobrega@gmail.com>
2020-07-06 09:52:03 -0300

    Organizando os dois projetos

---
+++ Unidade1/aula5/Mensagem.h
@@ -0,0 +1,24 @@
+#include <string>
+
+using namespace std;
+
+struct Hora
+{
+	int hora;
+  	int minuto;
+  	int segundo;
+};
+
+struct Data
+{
+	int dia;
+  	int mes;
+  	int ano;
+};
+
+struct Mensagem
+{
+	Hora hora;
+	Data data;
+	string conteudo;
+};
